dda040d82f5ccda6bc7fb79d2b498ae216726e8a
Evgeny Novokshanov <evgeny.novokshanov@gmail.com>
2024-03-19 00:11:49 +0300

    Добавлен еще один вариант решения task7

--- tasks/task7/main.go
+++ tasks/task7/main.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 )
 
-func main() {
+func variant1() {
 
 	var m sync.Map // для конкурентной записи в map следует использовать либо самописную структуру, работающую на основе мьютексов, либо sync.Map
 	var wg sync.WaitGroup
@@ -25,4 +25,60 @@ func main() {
 		return true
 	})
 
+}
+
+type MyMap struct {
+	mx sync.RWMutex
+	m  map[int]int
+}
+
+func NewMyMap() *MyMap {
+	m := MyMap{
+		m: make(map[int]int),
+	}
+	return &m
+}
+
+func (c *MyMap) Load(key int) (int, bool) {
+	c.mx.RLock()
+	defer c.mx.RUnlock()
+	val, ok := c.m[key]
+	return val, ok
+}
+
+func (c *MyMap) Print() {
+	c.mx.RLock()
+	defer c.mx.RUnlock()
+	for key, val := range c.m {
+		fmt.Printf("Ключ: %v, Значение: %v\n", key, val)
+	}
+}
+
+func (c *MyMap) Store(key int, value int) {
+	c.mx.Lock()
+	defer c.mx.Unlock()
+	c.m[key] = value
+}
+
+func variant2() {
+
+	m := NewMyMap() // для конкурентной записи в map следует использовать либо самописную структуру, работающую на основе мьютексов, либо sync.Map
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) { // запуск горутины для конкурентной записи в map
+			defer wg.Done()
+			m.Store(i, i*i) // Запись данных в map
+		}(i)
+	}
+
+	wg.Wait()
+
+	m.Print()
+
+}
+
+func main() {
+	variant2()
 }
